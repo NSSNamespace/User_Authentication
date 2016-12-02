292eb69efd5ca0d1a6279d91f84ee04b8c3ca4b9
Elliott Williams <ewilliamswtn@gmail.com>
2016-12-02 09:41:14 -0800

    turn down the base

--- src/User_Authentication/Models/ProductViewModels/ProductTypes.cs
+++ src/User_Authentication/Models/ProductViewModels/ProductTypes.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 namespace User_Authentication.Models.ProductViewModels
 {
     //Create ProductTypesViewModel that inherits from BaseViewModel 
-    public class ProductTypesViewModel : BaseViewModel
+    public class ProductTypesViewModel
     {
         public IEnumerable<Product> Products { get; set; }
         public IEnumerable<ProductType> ProductTypes { get; set; }
@@ -18,6 +18,6 @@ namespace User_Authentication.Models.ProductViewModels
         public IEnumerable<ProductTypeSubCategory> ProductTypeSubCategories { get; set; }
 
         //Create a custom constructor that accepts BangazonContext as an argument and passes that context (session with db) up to the methods on BaseViewModel
-        public ProductTypesViewModel(ApplicationDbContext ctx) : base(ctx) { }
+        public ProductTypesViewModel(ApplicationDbContext ctx) { }
     }
 }
