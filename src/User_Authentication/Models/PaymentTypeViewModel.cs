f7bd264358e0f74c079bad670322925e37ef2650
Elizabeth Sanger <libsang@gmail.com>
2016-12-02 11:39:07 -0600

    added payment type view model

---
+++ src/User_Authentication/Models/PaymentTypeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using User_Authentication.Data;
+using User_Authentication.Models;
+
+namespace User_Authentication.Models.ViewModels
+{
+    public class PaymentTypeViewModel    
+    {
+
+        public PaymentType PaymentType { get; set; }
+        public PaymentTypeViewModel(ApplicationDbContext ctx){ }
+    }
+}
