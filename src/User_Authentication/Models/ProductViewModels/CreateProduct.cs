684130c65775cafcd78c885d446f5a0555b05a98
Elizabeth Sanger <libsang@gmail.com>
2016-12-02 11:02:37 -0600

    completed product create view model

--- src/User_Authentication/Models/ProductViewModels/CreateProduct.cs
+++ src/User_Authentication/Models/ProductViewModels/CreateProduct.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using User_Authentication.Models;
+using User_Authentication.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace User_Authentication.Models.ProductViewModels
 {
     public class CreateProduct
     {
+        public Product Product { get; set; }
+        public List<SelectListItem> ProductTypeId { get; set; }
+
+        public CreateProduct(ApplicationDbContext ctx)
+        {
+
+            this.ProductTypeId = ctx.ProductType
+                                    .OrderBy(l => l.Label)
+                                    .AsEnumerable()
+                                    .Select(li => new SelectListItem
+                                    {
+                                        Text = li.Label,
+                                        Value = li.ProductTypeId.ToString()
+                                    }).ToList();
+
+            this.ProductTypeId.Insert(0, new SelectListItem
+            {
+                Text = "Choose category...",
+                Value = "0"
+            });
+        }
     }
 }
